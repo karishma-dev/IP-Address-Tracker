e18fa051daa7613913f6b559886d5430cc699808
Krrish105 <jainkarishma76@gmail.com>
2022-02-22 18:24:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [https://github.com/Krrish105/IP-Address-Tracker](https://github.com/Krrish105/IP-Address-Tracker)
+- Live Site URL: [https://krrish105.github.io/IP-Address-Tracker/](https://krrish105.github.io/IP-Address-Tracker/)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+## Author
+
+- Frontend Mentor - [Krrish105](https://www.frontendmentor.io/profile/Krrish105)
+- Twitter - [_karishma10](https://twitter.com/_karishma10)
+- Codepen - [Krrish105](https://codepen.io/krrish105)
+
---
+++ script.js
@@ -0,0 +1,56 @@
+// Variables
+const ipAddress = document.getElementById("ipAddress");
+const ipLocation = document.getElementById("location");
+const timezone = document.getElementById("timezone");
+const isp = document.getElementById("isp");
+const form = document.forms.ip;
+var map = L.map('map');
+
+// Eventlistener to call ipInfo function when the user enters the ip address
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    ipInfo(e.target[0].value);
+    e.target[0].value = "";
+})
+
+// inInfo function to fetch the data of the ip Address/domain inputted by the user
+const ipInfo = (address = "") => {
+    fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_qoJBZrBxMVeTgrJkk7P3vZIbtmb0U&ipAddress=${address}`)
+    .then(res => res.json())
+    .then(data => {
+        let lat = data.location.lat;
+        let lng = data.location.lng;
+        showIpInfo(data);
+        generateMap(lat,lng);
+    })
+}
+
+// Invoke the ipInfo function when the page loads
+ipInfo();
+
+// showIpInfo function to show to info of the ip address to the user
+const showIpInfo = (data) => {
+    ipAddress.innerHTML = data.ip;
+    ipLocation.innerHTML = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
+    timezone.innerHTML = `UTC ${data.location.timezone}`;
+    isp.innerHTML = data.isp;
+}
+
+// generateMap function to generate map aaccording to the latitude and longitude entered
+const generateMap = (lat, lng) => {
+    let locationIcon = L.icon({
+        iconUrl: 'images/icon-location.svg',
+    
+        iconSize:     [30, 30], // size of the icon
+        iconAnchor:   [lat, lng], // point of the icon which will correspond to marker's location
+        popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    });
+    map.setView([lat, lng], 17);
+    
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    }).addTo(map);
+
+    L.marker([lat, lng], {icon: locationIcon}).addTo(map);
+}
+
